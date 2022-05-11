c8eb62d9e09609e663ecfc6e59a045691d1b55f5
non-97 <nonmonaka07@gmail.com>
2022-05-12 08:13:41 +0900

    Initial commit

---
+++ test/ipv6-privatelink.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ipv6Privatelink from '../lib/ipv6-privatelink-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ipv6-privatelink-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ipv6Privatelink.Ipv6PrivatelinkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
